c9fa0f763dbe5a60eaa411770bd78c44aa07d47e
BippyMiester <admin@jdswebservice.com>
2024-01-19 00:15:06 -0500

    Updated Plugin Download Link

--- RustAnalytics.cs
+++ RustAnalytics.cs
@@ -30,9 +30,9 @@ namespace Oxide.Plugins
         // Plugin Metadata
         private const string _PluginName = "RustAnalytics";
         private const string _PluginAuthor = "BippyMiester";
-        private const string _PluginVersion = "0.0.35";
+        private const string _PluginVersion = "0.0.36";
         private const string _PluginDescription = "Official Plugin for RustAnalytics.com";
-        private const string _PluginDownloadLink = "INSERT_LINK_HERE";
+        private const string _PluginDownloadLink = "https://codefling.com/plugins/rustanalytics";
         private const string _PluginWebsite = "https://rustanalytics.com/";
 
         // Plugin References
